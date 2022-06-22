b7367343d3f85b87045a7ba20b6298d7e445ecbd
Trezlor <77504889+Trezlor@users.noreply.github.com>
2022-06-22 21:06:27 +0200

    hoi

--- funny.js
+++ funny.js
@@ -10,29 +10,29 @@ let arrow = document.getElementById("arrow_up");
 let img_rap = document.getElementById("img_rap");
 
 trigger.onmouseenter = function () {
-   audio.play();
+	audio.play();
 };
 
 trigger.onmouseleave = function () {
-   audio.pause();
-   audio.currentTime = 0;
+	audio.pause();
+	audio.currentTime = 0;
 };
 
 sqr_top.onmousedown = function () {
-   trigger.style.backgroundColor = "#dcdcdc";
-   heya.style.color = "#dcdcdc";
-   sqr_top.style.backgroundColor = "#1a1a20";
-   baya.style.color = "#1a1a20";
-   trigger.style.gridRowStart = 1;
-   trigger.style.gridColumn = 4;
-   red.style.gridRowStart = 1;
-   red.style.gridColumn = 2;
-   blue.style.gridRowStart = 1;
-   blue.style.gridColumn = 6;
-   arrow.style.gridRowStart = 2;
-   arrow.style.gridColumn = 4;
-   arrow.style.color = "#dcdcdc";
-   wrap.style.grid;
-   wrap.style.gridTemplateRows = "100px 40px";
-   img_rap.style.opacity = "1";
+	trigger.style.backgroundColor = "#dcdcdc";
+	heya.style.color = "#dcdcdc";
+	sqr_top.style.backgroundColor = "#1a1a20";
+	baya.style.color = "#1a1a20";
+	trigger.style.gridRowStart = 1;
+	trigger.style.gridColumn = 4;
+	red.style.gridRowStart = 1;
+	red.style.gridColumn = 2;
+	blue.style.gridRowStart = 1;
+	blue.style.gridColumn = 6;
+	arrow.style.gridRowStart = 2;
+	arrow.style.gridColumn = 4;
+	arrow.style.color = "#dcdcdc";
+	wrap.style.grid;
+	wrap.style.gridTemplateRows = "100px 40px";
+	img_rap.style.opacity = "1";
 };
